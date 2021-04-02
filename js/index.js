08723fc40c4581e4da5e818b1dbed70fcf3b3529
alyssuhjoyy <72486343+alyssuhjoyy@users.noreply.github.com>
2021-04-02 11:42:21 -0700

    update

--- js/index.js
+++ js/index.js
@@ -25,3 +25,25 @@ scrollToTopButton.onclick = function (e) {
     e.preventDefault();
     scrollToTop();
 }
+
+var tabButtons = document.querySelectorAll(".tabContainer .buttonContainer button");
+var tabPanels = document.querySelectorAll(".tabContainer .tabPanel");
+
+function showPanel(panelIndex,colorCode) {  
+    tabButtons.forEach(function(node){
+            node.style.backgroundColor="";
+            node.style.color="";
+    });
+    tabButtons[panelIndex].style.backgroundColor=colorCode;
+    tabButtons[panelIndex].style.color="white";
+    tabPanels.forEach(function(node) {
+             node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+    tabPanels[panelIndex].style.backgroundColor=colorCode;
+
+    node.style.backgroundColor = "";
+    node.style.color = "";
+};
+
+showPanel (0,'#F69F8E')
